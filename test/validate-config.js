0b8b1494a6c8bee26fe1ea2c0ea091dcb4bfda75
Athos Garcia <athospg@outlook.com>
2021-02-19 22:26:05 -0300

    ESLint config

---
+++ test/validate-config.js
@@ -0,0 +1,26 @@
+const eslint = require("eslint");
+const test = require("tape");
+
+test("load config in eslint to validate all rule syntax is correct", async t =>
+{
+  const CLIEngine = eslint.ESLint;
+
+  const cli = new CLIEngine({
+    useEslintrc       : false,
+    overrideConfigFile: "eslintrc.json",
+  });
+
+  const code = `
+const foo = 1;
+const bar = function(fo)
+{
+  return fo ** 2;
+};
+bar(foo);`;
+
+  const lintResult = await cli.lintText(code);
+
+  t.equal(lintResult.length, 1);
+  t.equal(lintResult[0].errorCount, 0);
+  t.end();
+});
